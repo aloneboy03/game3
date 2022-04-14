7684a032ef8d083edf08aaded7e19aec08ca9828
aloneboy <95104282+aloneboy03@users.noreply.github.com>
2022-04-15 01:51:17 +0500

    style 2

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const selectedUserHand = document.querySelector('fa-hand-back-fist');
 const selectedCompHand = document.querySelector('fa-hand-back-fist');
 const wonText = document.querySelector('.won--text');
 const restartGame = document.querySelector('.restart--game');
+const selectUser = document.querySelector('.user--selected');
+const selectComp = document.querySelector('.comp--selected');
 const restNum = 0;
 const UserResNum = 0;
 const CompResNum = 0;
+
 
 restartGame.addEventListener('click', function(e){
     resultUser.textContent = restNum;
@@ -20,13 +23,16 @@ restartGame.addEventListener('click', function(e){
 });
 
 firstHand.addEventListener('click', function(a){
-    
+    // let html = `<i class="fa-solid fa-hand-back-fist"></i>`;
+    // selectUser.insertAdjacentHTML('afterbegin', html);
 });
 
-secondHand.addEventListener('click', function(a){
-
+secondHand.addEventListener('click', function(b){
+    // let html = `<i class="fa-solid fa-hand"></i>`;
+    // selectUser.insertAdjacentHTML('afterbegin', html);
 });
 
-thirdHand.addEventListener('click', function(a){
-
+thirdHand.addEventListener('click', function(c){
+    // let html = `<i class="fa-solid fa-hand-scissors"></i>`;
+    // selectUser.insertAdjacentHTML('afterbegin', html);
 });
